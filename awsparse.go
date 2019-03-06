12e9c8bfdfa29e74bb58d37b5cfbd4bc302722f1
James <jseconners@gmail.com>
2019-03-05 18:40:32 -0800

    Trying out CSV file reading

--- awsparse.go
+++ awsparse.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello")
+	datafile := os.Args[1]
+
+	csvFile, _ := os.Open(datafile)
+	reader := csv.NewReader(bufio.NewReader(csvFile))
+	reader.Comma = '\t'
+
+	for {
+		line, e := reader.Read()
+		if e == io.EOF {
+			break
+		} else if e != nil {
+			log.Fatal(e)
+		}
+		fmt.Println(line)
+	}
 }
