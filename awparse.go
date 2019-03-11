91db840515eb72099d2b1ed44f075c0b86e1109b
James <jseconners@gmail.com>
2019-03-11 13:19:13 -0700

    Framing out file walkthrough and parsing

--- awparse.go
+++ awparse.go
@@ -2,50 +2,89 @@ package main
 
 import (
 	"bufio"
-	"fmt"
+	"encoding/csv"
+	"github.com/urfave/cli"
+	"io"
 	"log"
 	"os"
-
-	"github.com/urfave/cli"
+	"path/filepath"
+	"sort"
+	"strconv"
 )
 
 
-func readHeader(fileName string) {
-	fh, _ := os.Open(fileName)
+var months = []string {
+	"January", "February", "March", "April", "May", "June",
+	"July", "August", "September", "October", "November", "December",
+}
 
-	s := bufio.NewScanner(fh)
-	for s.Scan() {
-		fmt.Println(s.Text())
-	}
+func fileExists(pathStr string) bool {
+	_, err := os.Stat(pathStr);
+	return !os.IsNotExist(err)
+}
 
-	/**
-	reader := csv.NewReader(bufio.NewReader(fh))
-	reader.Comma = '\t'
-	reader.FieldsPerRecord = -1
-	for {
-		line, e := reader.Read()
-		if e == io.EOF {
-			break
-		} else if e != nil {
-			log.Fatal(e)
+func dataFileDay(fileName string) int {
+	dayInt, _ := strconv.Atoi(fileName[6:8])
+	return dayInt
+}
+
+func makeCSV(dataDir string) {
+	yearDirs, _ := filepath.Glob(filepath.Join(dataDir, "/[1-2][0-9][0-9][0-9]"))
+	for _, yearDir := range yearDirs {
+		for _, month := range months {
+			monthDir := filepath.Join(yearDir, month)
+			if fileExists(monthDir) {
+				dataFiles, _ := filepath.Glob(filepath.Join(monthDir, "*.100"))
+
+				// Ensure we're parsing these files in order
+				fileMap := make(map[int]string)
+				dayInts := make([]int, 0, 30)
+				for _, df := range dataFiles {
+					di := dataFileDay(filepath.Base(df))
+					dayInts = append(dayInts, di)
+					fileMap[di] = df
+				}
+				sort.Ints(dayInts)
+
+				for _, di := range dayInts {
+					dataFile := fileMap[di]
+					fileHandle, _ := os.Open(dataFile)
+					reader := csv.NewReader(bufio.NewReader(fileHandle))
+					reader.Comma = '\t'
+					reader.FieldsPerRecord = -1
+
+					writer := csv.NewWriter(os.Stdout)
+					writer.Comma = ','
+
+					for {
+						line, error := reader.Read()
+						if error == io.EOF {
+							break
+						} else if error != nil {
+							log.Fatal(error)
+						}
+						writer.Write(line)
+					}
+					fileHandle.Close()
+					writer.Flush()
+				}
+			}
 		}
-		fmt.Println(line)
 	}
-	**/
+
 }
 
 
+
 func main() {
-
 	app := cli.NewApp()
-
 	app.Commands = []cli.Command{
 		{
-			Name:    "checkheader",
+			Name:    "csv",
 			Aliases: []string{"ch"},
-			Usage:   "Check header definition file",
+			Usage:   "Test command",
 			Action: func(c *cli.Context) error {
-				readHeader(c.Args().First())
+				makeCSV(c.Args().First())
 				return nil
 			},
 		},
