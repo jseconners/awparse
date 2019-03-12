9a5bec5d41e4d37ba4575ac55049a893c46c478b
James <jseconners@gmail.com>
2019-03-12 15:04:30 -0700

    Re-factor into functions a bit more and add progress bar testing

--- awparse.go
+++ awparse.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"encoding/csv"
+	"github.com/schollz/progressbar"
 	"github.com/urfave/cli"
 	"io"
 	"log"
@@ -36,13 +37,15 @@ func dataFileDay(fileName string) int {
 	return dayInt
 }
 
+
 /**
- * Get slice of data files sorted by dataFileDay() return
+ * Get a sorted slice of data file paths from a specified
+ * month's data directory
  */
-func getSortedDataFiles(dirName string, glob string) []string {
+func getSortedMonthFiles(dirName string, glob string) []string {
 	dataFiles, _ := filepath.Glob(filepath.Join(dirName, glob))
 	fileMap := make(map[int]string)
-	dayInts := make([]int, 0, 30)
+	dayInts := make([]int, 0, 31)
 
 	for _, df := range dataFiles {
 		di := dataFileDay(filepath.Base(df))
@@ -51,50 +54,64 @@ func getSortedDataFiles(dirName string, glob string) []string {
 	}
 	sort.Ints(dayInts)
 
-	sortedDataFiles := make([]string, 0, 30)
+	sortedDataFiles := make([]string, 0, 31)
 	for _, di := range dayInts {
 		sortedDataFiles = append(sortedDataFiles, fileMap[di])
 	}
 	return sortedDataFiles
 }
 
-
 /**
- * Parse data files and write generated CSV to stdout
+ * Get a sorted slice of all data file paths for a given data
+ * archive directory and file glob pattern
  */
-func makeCSV(dataDir string) {
-	yearDirs, _ := filepath.Glob(filepath.Join(dataDir, "/[1-2][0-9][0-9][0-9]"))
+func getAllSortedDataFiles(dirName string, glob string) []string {
+	allDataFiles := make([]string, 0, 100)
+	yearDirs, _ := filepath.Glob(filepath.Join(dirName, "/[1-2][0-9][0-9][0-9]"))
 	for _, yearDir := range yearDirs {
 		for _, month := range months {
 			monthDir := filepath.Join(yearDir, month)
 			if fileExists(monthDir) {
-
-				sortedDataFiles := getSortedDataFiles(monthDir, "*100")
-
-				for _, df := range sortedDataFiles {
-					fileHandle, _ := os.Open(df)
-					reader := csv.NewReader(bufio.NewReader(fileHandle))
-					reader.Comma = '\t'
-					reader.FieldsPerRecord = -1
-
-					writer := csv.NewWriter(os.Stdout)
-					writer.Comma = ','
-
-					for {
-						line, e := reader.Read()
-						if e == io.EOF {
-							break
-						} else if e != nil {
-							log.Fatal(e)
-						}
-						writer.Write(line)
-					}
-					fileHandle.Close()
-					writer.Flush()
-				}
+				allDataFiles = append(allDataFiles, getSortedMonthFiles(monthDir, glob)...)
 			}
 		}
 	}
+	return allDataFiles
+}
+
+
+/**
+ * Parse data files and write generated CSV to stdout
+ */
+func makeCSV(dataDir string, glob string) {
+	allDataFiles := getAllSortedDataFiles(dataDir, glob)
+	bar := progressbar.New(len(allDataFiles))
+
+	outFile, _ := os.Create("data.csv")
+	writer := csv.NewWriter(outFile)
+	writer.Comma = ','
+
+	for _, df := range allDataFiles {
+		bar.Add(1)
+
+		inFile, _ := os.Open(df)
+		reader := csv.NewReader(bufio.NewReader(inFile))
+		reader.Comma = '\t'
+		reader.FieldsPerRecord = -1
+
+		for {
+			line, e := reader.Read()
+			if e == io.EOF {
+				break
+			} else if e != nil {
+				log.Fatal(e)
+			}
+			writer.Write(line)
+		}
+		inFile.Close()
+	}
+	writer.Flush()
+	outFile.Close()
 }
 
 
@@ -108,7 +125,7 @@ func main() {
 			Aliases: []string{"ch"},
 			Usage:   "Test command",
 			Action: func(c *cli.Context) error {
-				makeCSV(c.Args().First())
+				makeCSV(c.Args().Get(0), c.Args().Get(1))
 				return nil
 			},
 		},
